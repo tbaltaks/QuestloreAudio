577139ae01aa4d7aba9ec0ef0271c57cd8742da8
Tom <tom@baltaks.com>
2025-03-05 13:49:36 +1100

    Committing all

---
+++ QuestloreAudio/QuestloreAudio/QuestloreAudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestloreAudioApp.swift
+//  QuestloreAudio
+//
+//  Created by Tom Baltaks on 5/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct QuestloreAudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
